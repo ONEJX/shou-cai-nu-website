140068b73cb54ed753c5738afde0fe0af77bf808
onejx111 <10970444+onejx111@user.noreply.gitee.com>
2022-06-13 18:49:57 +0800

    deploy

---
+++ precache-manifest.b6f7a6c2557a0d0699fc28cd59d993bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822372ad9196a82bf879",
+    "url": "/shou-cai-nu-website/css/app.9a4ce0a5.css"
+  },
+  {
+    "revision": "e8067c8e3413be91405c00baf41c45ab",
+    "url": "/shou-cai-nu-website/img/守财奴.e8067c8e.png"
+  },
+  {
+    "revision": "c2c509b44b9be11b8442c2fb9c22d981",
+    "url": "/shou-cai-nu-website/index.html"
+  },
+  {
+    "revision": "822372ad9196a82bf879",
+    "url": "/shou-cai-nu-website/js/app.779f0b02.js"
+  },
+  {
+    "revision": "4109daaca1f14dab1b53",
+    "url": "/shou-cai-nu-website/js/chunk-vendors.8118bfe6.js"
+  },
+  {
+    "revision": "21911bd4e118ad4374638ca125f0df40",
+    "url": "/shou-cai-nu-website/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/shou-cai-nu-website/robots.txt"
+  }
+]);
